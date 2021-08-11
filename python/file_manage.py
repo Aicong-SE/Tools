e4b6a9f689477ccabe5c843a7baea49de8ebb43b
xiaoai <aishaocong@163.com>
2021-08-11 14:10:23 +0800

    update

--- python/file_manage.py
+++ python/file_manage.py
@@ -7,6 +7,7 @@ import os
 from pathlib import Path
 
 
+# region 判断文件与目录是否存在
 def file_exists(url: str) -> bool:
     """
     检查指定的文件是否存在
@@ -34,15 +35,31 @@ def exists(url: str) -> bool:
     return Path(url).exists()
 
 
+# endregion
+
+# region 创建文件与目录
+def create_directory(url: str) -> None:
+    """
+    创建目录
+    创建目录时如果路径不存在会创建这个路径
+    :param url: 目录路径
+    """
+    os.makedirs(url)
+
+
 def create_file(url: str) -> None:
     """
     创建文件
     创建文件时如果路径不存在会创建这个路径
     :param url: 文件路径
     """
-    os.makedirs(url)
+    file = open(url, 'w')
+    file.close()
 
 
+# endregion
+
+# region 读写文件
 def write_file(url: str, mode: str, content: str):
     """
     将内容写入文件
@@ -54,3 +71,31 @@ def write_file(url: str, mode: str, content: str):
         raise Exception("请传入正确的 mode 值")
     with open(url, mode) as f:
         f.write(content)
+
+
+# endregion
+
+# region 文件名操作
+def get_file_name(url: str) -> list:
+    """
+    获取文件名
+    将指定目录路径下的所有 文件名与目录名 以列表形式返回
+    :param url: 目录路径
+    """
+    return os.listdir(url)
+
+
+def file_name_funcition():
+    """
+    关于文件名的一些方法
+    """
+    file_name = "E:/test/t.lib"
+    print(os.path.splitext(file_name))  # ('E:/test/t', '.lib')  拆分 文件名 和 后缀
+    print(os.path.split(file_name))  # ('E:/test', 't.lib')  拆分 路径 和 文件名
+
+
+# endregion
+
+
+if __name__ == '__main__':
+    file_name_funcition()
